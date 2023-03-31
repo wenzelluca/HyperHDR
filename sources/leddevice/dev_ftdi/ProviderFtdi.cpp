e4faeaa0ab59102494830113fd4f6e9b70b3ebcf
Ainur Timerbaev <ainur.timerbaev@superawesome.com>
2023-03-31 09:53:09 +0100

    Return delay before closing

--- sources/leddevice/dev_ftdi/ProviderFtdi.cpp
+++ sources/leddevice/dev_ftdi/ProviderFtdi.cpp
@@ -5,6 +5,9 @@
 #include <ftdi.h>
 #include <libusb.h>
 
+#include <QEventLoop>
+#include <QTimer>
+
 #define ANY_FTDI_VENDOR 0x0
 #define ANY_FTDI_PRODUCT 0x0
 
@@ -24,6 +27,8 @@ namespace Pin
 const unsigned char pinDirection = Pin::SK | Pin::DO | Pin::CS | Pin::L0;
 
 const QString ProviderFtdi::AUTO_SETTING = QString("auto");
+
+constexpr std::chrono::milliseconds CLOSE_DELAY_TIME{ 15 };
 
 ProviderFtdi::ProviderFtdi(const QJsonObject &deviceConfig)
 	: LedDevice(deviceConfig),
@@ -148,6 +153,11 @@ int ProviderFtdi::close()
 	if (_ftdic != NULL)
 	{
 		Debug(_log, "Closing FTDI device");
+        QEventLoop loop;
+//        Delay to give time to push color black from writeBlack() into the led
+        QTimer::singleShot(CLOSE_DELAY_TIME.count(), &loop, &QEventLoop::quit);
+        loop.exec();
+
 		ftdi_set_bitmode(_ftdic, 0x00, BITMODE_RESET);
 		ftdi_usb_close(_ftdic);
 		_ftdic = NULL;
